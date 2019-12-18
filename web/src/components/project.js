fba2080bc57daf5bdcd6431f7055c81686f194e6
Guillermo Martinez <guillermo@thejibe.com>
2019-12-17 16:41:32 -0800

    test forms

--- web/src/components/project.js
+++ web/src/components/project.js
@@ -69,6 +69,7 @@ function Project (props) {
           </aside>
         </div>
         <form name="contact" method="POST" data-netlify="true">
+          <input type="hidden" name="form-name" value="contact" />
           <p>
             <label>Your Name: <input type="text" name="name" /></label>   
           </p>
